e25bbc0f763c90fce96d2894e0db2eb49d8dfc62
Steve Mckellar <steve@wwqrd.uk>
2018-12-06 16:36:51 +0000

    Working filter function

--- __tests__/filter.test.js
+++ __tests__/filter.test.js
@@ -6,10 +6,10 @@ const generateRuleConfig = helpers.generateRuleConfig;
 
 const network = {
   nodes: [
-    generateNode({ name: 'William', age: 19 }),
-    generateNode({ name: 'Theodore', age: 18 }),
-    generateNode({ name: 'Rufus', age: 51 }),
-    generateNode({ name: 'Phone Box' }, 'public_utility'),
+    generateNode({ name: 'William', age: 19, favouriteColor: 'green' }),
+    generateNode({ name: 'Theodore', age: 18, favouriteColor: 'red' }),
+    generateNode({ name: 'Rufus', age: 51, favouriteColor: 'red' }),
+    generateNode({ name: 'Phone Box' }, 'publicUtility'),
   ],
   edges: [
     { from: 1, to: 2, type: 'friend' },
@@ -36,7 +36,69 @@ describe('filter', () => {
 
       const filter = getFilter(filterConfig);
       const result = filter(network);
+      expect(result.nodes.length).toEqual(2);
+    });
+  });
+
+  describe('OR', () => {
+    const filterConfig = {
+      rules: [
+        generateRuleConfig('alter', {
+          type: 'person',
+          operator: 'LESS_THAN',
+          attribute: 'age',
+          value: 20,
+        }),
+        generateRuleConfig('alter', {
+          type: 'publicUtility',
+          operator: 'EXISTS',
+        }),
+      ],
+      join: 'OR',
+    }
+
+    const filter = getFilter(filterConfig);
+
+    it('matches are combined', () => {
+      const result = filter(network);
       expect(result.nodes.length).toEqual(3);
+    });
+
+    it('orphaned edges are removed', () => {
+      const result = filter(network);
+      expect(result.edges.length).toEqual(2);
+    });
+  });
+
+  describe('AND', () => {
+    const filterConfig = {
+      rules: [
+        generateRuleConfig('alter', {
+          type: 'person',
+          operator: 'LESS_THAN',
+          attribute: 'age',
+          value: 20,
+        }),
+        generateRuleConfig('alter', {
+          type: 'person',
+          operator: 'EXACTLY',
+          attribute: 'favouriteColor',
+          value: 'red',
+        }),
+      ],
+      join: 'AND',
+    }
+
+    const filter = getFilter(filterConfig);
+
+    it('matches are refined', () => {
+      const result = filter(network);
+      expect(result.nodes.length).toEqual(1);
+    });
+
+    it('orphaned edges are removed', () => {
+      const result = filter(network);
+      expect(result.edges.length).toEqual(0);
     });
   });
 });
--- filter.js
+++ filter.js
@@ -1,4 +1,38 @@
-const filter = ({ rules, join }) =>
-  network => network;
+const buildEdgeLookup = require('./buildEdgeLookup');
+const nodePrimaryKeyProperty = require('./nodePrimaryKeyProperty');
+const getRule = require('./rules').default;
+
+// remove orphaned edges
+const trimEdges = (network) => {
+  const uids = new Set(network.nodes.map(node => node[nodePrimaryKeyProperty]));
+
+  const edges = network.edges.filter(
+    ({ from, to }) => uids.has(from) && uids.has(to),
+  );
+
+  return {
+    ...network,
+    edges,
+  };
+}
+
+const filter = ({ rules, join }) => {
+  const ruleRunners = rules.map(getRule);
+
+  return (network) => {
+    const joinType = join === 'AND' ? 'every' : 'some'; // use the built-in methods
+
+    const edgeMap = buildEdgeLookup(network.edges);
+
+    const nodes =  network.nodes.filter(
+      node => ruleRunners[joinType](rule => rule(node, edgeMap)),
+    );
+
+    return trimEdges({
+      ...network,
+      nodes,
+    });
+  };
+}
 
 exports.default = filter;
