88d05d088f0d7bf6bd27c6862a7987967e3ed180
Steve Mckellar <steve@wwqrd.uk>
2018-12-06 13:38:53 +0000

    Alter type rules

--- __tests__/rules.test.js
+++ __tests__/rules.test.js
@@ -1,45 +1,67 @@
 /* eslint-env jest */
+const nodeAttributesProperty = require('../nodeAttributesProperty');
 const getRule = require('../rules').getRule;
 
 const nodes = [
-  { type: 'person', attributes: { name: 'William' } },
-  { type: 'person', attributes: { name: 'Theodore' } },
+  { type: 'person', [nodeAttributesProperty]: { name: 'William' } },
+  { type: 'person', [nodeAttributesProperty]: { name: 'Theodore' } },
+  { type: 'public_utility', [nodeAttributesProperty]: { name: 'Phone Box' } },
 ];
 
 describe('rules', () => {
   it('getRule() returns a function', () => {
-
-    const ruleConfig = {
-      type: 'alter',
-      options: {
-        "type": "person",
-        "attribute": "name",
-        "operator": "EXACTLY",
-        "value": "William"
-      }
-    };
-
-    const rule = getRule(ruleConfig);
+    const rule = getRule({});
 
     expect(typeof rule).toHaveReturned
   });
 
 
-  it('alter rule', () => {
+  describe('alter rule', () => {
+    describe('type rules', () => {
+      it('EXISTS', () => {
+        const ruleConfig = {
+          type: 'alter',
+          options: {
+            "type": "person",
+            "operator": "EXISTS",
+          }
+        };
 
-    const ruleConfig = {
-      type: 'alter',
-      options: {
-        "type": "person",
-        "attribute": "name",
-        "operator": "EXACTLY",
-        "value": "William"
-      }
-    };
+        const rule = getRule(ruleConfig);
+        const matches = nodes.filter(rule);
+        expect(matches.length).toEqual(2);
+      });
 
-    const rule = getRule(ruleConfig);
+      it('NOT_EXISTS', () => {
+        const ruleConfig = {
+          type: 'alter',
+          options: {
+            "type": "person",
+            "operator": "NOT_EXISTS",
+          }
+        };
 
-    expect(rule(nodes[0])).toEqual(false);
-    expect(rule(nodes[1])).toEqual(true);
+        const rule = getRule(ruleConfig);
+        const matches = nodes.filter(rule);
+        expect(matches.length).toEqual(1);
+      });
+    });
+
+    // it('correctly matches attribute rules', () => {
+    //   const ruleConfig = {
+    //     type: 'alter',
+    //     options: {
+    //       "type": "person",
+    //       "attribute": "name",
+    //       "operator": "EXACTLY",
+    //       "value": "William"
+    //     }
+    //   };
+
+    //   const rule = getRule(ruleConfig);
+
+    //   expect(rule(nodes[0])).toEqual(false);
+    //   expect(rule(nodes[1])).toEqual(true);
+    // });
   });
 });
--- rules.js
+++ rules.js
@@ -1,10 +1,24 @@
-const alterRule = (options) =>
-  () => {};
+const nodeAttributesProperty = require('./nodeAttributesProperty');
+const predicate = require('./predicate').default;
+
+const alterRule = ({ attribute, operator, type, value }) =>
+  (node) => {
+    if (!attribute) {
+      switch (operator) {
+        case 'EXISTS':
+          return node.type === type;
+        default:
+          return node.type != type;
+      }
+    }
+  };
 
 const getRule = (ruleConfig) => {
   switch(ruleConfig.type) {
     case 'alter':
       return alterRule(ruleConfig.options);
+    default:
+      () => true;
   }
 }
 
