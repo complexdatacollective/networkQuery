47422a2e90a7aedb9388145f73cfeea56a85f1df
Steve Mckellar <steve@wwqrd.uk>
2018-12-06 17:52:28 +0000

    Add top level documentation

--- filter.js
+++ filter.js
@@ -16,6 +16,39 @@ const trimEdges = (network) => {
   };
 }
 
+/**
+ * Returns a method which can filter the network.
+ * The returned method takes a network object and returns a network object
+ *
+ * @param filter
+ * @param {Object[]} filter.rules An array of rule options
+ * @param {('alter'|'edge')} filter.rules[].type What the rule will act on
+ * @param {Object} filter.rules[].options The parameters of the rule
+ * @param {('AND'|'OR')} filter.join The method used to combine rule outcomes
+ *
+ * Example usage:
+ *
+ * ```
+ * import getFilter from 'networkQuery/filter';
+ *
+ * const config = {
+ *   rules: [
+ *     {
+ *       type: 'alter',
+ *       options: { type: 'person', attribute: 'name', operator: 'EXACTLY', value: 'Bill'},
+ *     },
+ *     {
+ *       type: 'edge',
+ *       options: { type: 'friend', operator: 'EXISTS' },
+ *     },
+ *   ],
+ *   join: 'AND',
+ * };
+ *
+ * const filter = getFilter(config);
+ * const result = filter(network);
+ */
+
 const filter = ({ rules, join }) => {
   const ruleRunners = rules.map(getRule);
   const joinType = join === 'AND' ? 'every' : 'some'; // use the built-in methods
--- query.js
+++ query.js
@@ -3,6 +3,41 @@ const nodePrimaryKeyProperty = require('./nodePrimaryKeyProperty');
 const getRule = require('./rules').default;
 const predicate = require('./predicate').default;
 
+/**
+ * Returns a method which can query the network.
+ * The returned method takes a network object as an argument and returns a boolean.
+ *
+ * @param query
+ * @param {Object[]} query.rules An array of rule options
+ * @param {('ego'|'alter','edge')} query.rules[].type What the rule will act on
+ * @param {Object} query.rules[].options The parameters of the rule
+ * @param {Object} query.rules[].count The parameters used to assess the rule outcome (unless type is 'ego')
+ * @param {('AND'|'OR')} query.join The method used to combine rule outcomes
+ *
+ * Example usage:
+ *
+ * ```
+ * import getQuery from 'networkQuery/query';
+ *
+ * const config = {
+ *   rules: [
+ *     {
+ *       type: 'alter',
+ *       options: { type: 'person', attribute: 'name', operator: 'EXACTLY', value: 'Bill'},
+ *       count: { operator: 'GREATER_THAN', value: 0 },
+ *     },
+ *     {
+ *       type: 'ego',
+ *       options: { attribute: 'name', operator: 'EXACTLY', value: 'Bill'},
+ *     },
+ *   ],
+ *   join: 'AND',
+ * };
+ *
+ * const query = getQuery(config);
+ * const result = query(network);
+ */
+
 const query = ({ rules, join }) => {
   const joinType = join === 'AND' ? 'every' : 'some'; // use the built-in methods
 
